95468668f57d9aff0f6adeac28665f14a565ee59
constGregor <margutti.silvia1@gmail.com>
2024-02-13 16:20:16 +0100

    deleted app.css

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./Hooks/useGames";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
-import Emoji from "./components/Emoji";
 
 export interface GameQuery {
   genre: Genre | null;
